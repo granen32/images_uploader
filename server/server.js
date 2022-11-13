ec42679f66e789e6e38a4d0c78e74bf090406c2c
rnjsrbgud@naver.com <rnjsrbgud@naver.com>
2022-11-13 17:21:12 +0900

    node 서버 이미지 업로드

--- server/server.js
+++ server/server.js
@@ -1,9 +1,36 @@
 const express = require("express");
 const multer = require("multer");
-const upload = multer({ dest: "uploads" });
+const { v4: uuid } = require("uuid");
+const mime = require("mime-types");
+// multer 저장경로 및 네임 설정
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "./uploads"),
+  // uuid 뒤에 mime-types을 활용하여 이미지 타입 설정
+  filename: (req, file, cb) =>
+    cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
+});
+// 업로드 설정
+const upload = multer({
+  storage,
+  fileFilter: (req, res, cb) => {
+    if (["images/jpeg", "images/png"]) {
+      cb(null, true);
+    } else {
+      cb(new Error("invaid file type", false));
+    }
+  },
+  limits: {
+    fileSize: 1024 * 1024 * 10,
+  },
+});
 
 const app = express();
 const PORT = 5001;
+
+// uploads 폴더에 접근 가능하게 하기
+
+app.use("/uploads", express.static("uploads"));
+
 // 라우터 만들기 이미지 업로드
 // 함수 인자로 리퀘스트 바디 리스폰스 바디
 app.post("/upload", upload.single("imageTest"), (req, res) => {
