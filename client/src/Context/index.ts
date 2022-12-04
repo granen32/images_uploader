2524dde38753b6382a4a41e7d0dd861592eb469e
rnjsrbgud@naver.com <rnjsrbgud@naver.com>
2022-12-04 17:02:31 +0900

    클라이언트 이미지 디스플레이

--- client/src/Context/index.ts
+++ client/src/Context/index.ts
@@ -1,22 +1,7 @@
 import React, { createContext } from "react";
 
-type ImageState = {
-  _id?: string;
-  key?: string;
-  originalFileName?: string;
-  createdAt?: string;
-  updatedAt?: string;
-};
-const ImageStateProps = {
-  _id: "",
-  key: "",
-  originalFileName: "",
-  createdAt: "",
-  updatedAt: "",
-};
+export const ImageContext = createContext();
 
-export const ImageConText = createContext<ImageState | undefined>(
-  ImageStateProps
-);
-
-export const ImageProvider = ImageConText.Provider;
+export const ImageProvider = (props) => {
+  return <ImageContext.Provider>{prop.children}</ImageContext.Provider>;
+};
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GlobalStyle } from "./styles";
-import { ImageProvider } from "./Context";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ImageProvider value={undefined}>
-      <GlobalStyle />
-      <App />
-    </ImageProvider>
+    <GlobalStyle />
+    <App />
   </React.StrictMode>
 );
